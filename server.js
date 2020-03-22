ade012938161b9e2b1bb58ae5ebc5327a456b9b5
Shashank  Shahare <srshahare22@gmail.com>
2020-03-22 12:55:34 -0700

    changed database url

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ var image = require('./controllers/image');
 const db = knex({
     client: 'pg',
     connection: {
-      host : process.env.DATABASE_URL,
+      connectionString : process.env.DATABASE_URL,
       ssl: true,
     }
   });
