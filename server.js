9474c7575a793fda0804c78f0f311a5aadd5f2af
Shashank  Shahare <srshahare22@gmail.com>
2020-03-22 12:51:34 -0700

    changed database url

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ var image = require('./controllers/image');
 const db = knex({
     client: 'pg',
     connection: {
-      host : '127.0.0.1',
+      host : 'postgresql-sinuous-64125',
       user : 'postgres',
       password : 'root',
       database : 'smartbrain'
